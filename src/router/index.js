184534cc3c95b1bb9e05aff6e8f2b1af640fff8f
fqniu <1966798438@qq.com>
2021-06-14 12:53:55 +0800

    路由模式history

--- src/router/index.js
+++ src/router/index.js
@@ -9,8 +9,7 @@ import pageNone from '@/views/pageNone'
 
 Vue.use(Router)
 export default new Router({
-  // mode: 'history',
-
+  mode: 'history',
   routes: [{
       path: '/login',
       name: 'login',
